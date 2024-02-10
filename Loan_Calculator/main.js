5cda2df27697ca2b13c8bbab2c3924bcccc3d4b6
= <=>
2024-02-10 20:45:25 +0000

    added new project

---
+++ Loan_Calculator/main.js
@@ -0,0 +1,5 @@
+document.getElementById("calculateBtn").addEventListener('click', calculateLoan);
+
+function calculateLoan(){
+    const loanAmount = parseFloat(document.getElementById("loan-amount").value());
+}
